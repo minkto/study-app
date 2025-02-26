72cd83f86da0c0406b15df8421e1dfabc71d75e2
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-02-26 16:23:17 +0000

    [SA-34]: Added Dropdown logic for Resource Card.

--- src/components/resource-listings-card/ResourceListingsCard.tsx
+++ src/components/resource-listings-card/ResourceListingsCard.tsx
@@ -1,20 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
 import CategoryPill from '../category-pill/CategoryPill';
 import IconMoreHorizontal from '../icons/icon-more-horizontal/IconMoreHorizontal';
 import styles from './resource-listings-card.module.css'
 
-interface ResourceListingsCardProps 
-{
-    title? : string;
+interface ResourceListingsCardProps {
+    title?: string;
 }
 
-const ResourceListingsCard = ({title = "Card Name"} : ResourceListingsCardProps) => 
-{
-    return(
+const ResourceListingsCard = ({ title = "Card Name" }: ResourceListingsCardProps) => {
+    const [dropdownMenuModal, setDropdownMenuModal] = useState(false);
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    const onModalClick = () => dropdownMenuModal ? 
+    setDropdownMenuModal(false) : 
+    setDropdownMenuModal(true);
+
+    const onOutsideClick = (e : MouseEvent|KeyboardEvent ) => {
+        if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+            setDropdownMenuModal(false);
+        }
+    }
+
+    const showDropdownMenu = () => {
+        return <div className={styles['card-dropdown-menu']} ref={modalRef}>
+            <ul className={styles['card-dropdown-menu__list']}>
+                <li className={styles['card-dropdown-menu__list-item']}>Option 1</li>
+                <li className={styles['card-dropdown-menu__list-item']}>Option 2</li>
+                <li className={styles['card-dropdown-menu__list-item']}>Option 3</li>
+            </ul>
+        </div>
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousedown", onOutsideClick);
+        document.addEventListener("keydown", onOutsideClick);
+    
+        // Cleanup function
+        return () => {
+          document.removeEventListener("mousedown", onOutsideClick);
+          document.addEventListener("keyup", onOutsideClick);
+        };
+      }, []);
+
+    return (
         <div className={styles["resources-listing-card"]}>
             <div className={styles["resources-listing-card__row"]}>
                 <h2 className={styles["resources-listing-card__name"]}>{title}</h2>
                 <div className={styles["resources-listing-card__options"]}>
-                    <button>
+                    {dropdownMenuModal ? showDropdownMenu() : null}
+                    <button className={styles["resources-listing-card__options-button"]} onClick={onModalClick}>
                         <IconMoreHorizontal width={32} height={32} />
                     </button>
                 </div>
@@ -51,7 +85,7 @@ const ResourceListingsCard = ({title = "Card Name"} : ResourceListingsCardProps)
 
             <div className={styles["resources-listing-card__description-overlay"]}></div>
         </div>
-   );
+    );
 }
 
 export default ResourceListingsCard;
