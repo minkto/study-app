3ad943b41076cc2c3622dc3f1f23d7226dd1b7a4
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-02-28 15:59:01 +0000

    [SA-45]: Added Statuses int chapters.

--- SQL Scripts/Create Chapters Table Script.sql
+++ SQL Scripts/Create Chapters Table Script.sql
@@ -2,11 +2,18 @@ CREATE TABLE chapters
 (
   chapter_id SERIAL PRIMARY KEY NOT NULL,
   resource_id INT,
+  status_id INT,
   name TEXT NOT NULL,
   url VARCHAR(800),
   original_date_completed timestamp,
   last_date_completed timestamp,
+  
   CONSTRAINT fk_resource_id 
   	FOREIGN KEY (resource_id) 
-	  REFERENCES resources(resource_id)
+	  REFERENCES resources(resource_id),
+
+
+  CONSTRAINT fk_statuses_id 
+  	FOREIGN KEY (status_id) 
+	  REFERENCES statuses(status_id)
 );
