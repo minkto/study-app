c6954268aa11b2cbf45be58b5c1e02142804e3ae
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-03-11 15:01:38 +0000

    [SA-73]: Added Button to add resources and chapters.

--- src/app/dashboard/resources/page.tsx
+++ src/app/dashboard/resources/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { DashboardWidget } from '@/components/dashboard/DashboardWidget';
+import IconPlus from '@/components/icons/icon-more-horizontal/IconPlus';
 import ResourceListings from '@/components/resource-listings/ResourceListings';
+import Link from 'next/link';
 
 const Page = () => {
+
     return (
         <div>
             <h1>Resources Page</h1>
             <DashboardWidget title="Resources">
+                <div className='dashboard__widget-row row-reverse'>
+                    <Link className='dashboard-primary-btn' href={'resources/add-resource'}><IconPlus width={24} height={24}/>Add</Link>
+                </div>
                 <ResourceListings />
             </DashboardWidget>
         </div>)
