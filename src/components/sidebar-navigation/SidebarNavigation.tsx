c935d6429cea364b37fd8b3311be38b20958eb5e
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-03-12 13:06:41 +0000

    [SA-15]: Added expand function for side nav.

--- src/components/sidebar-navigation/SidebarNavigation.tsx
+++ src/components/sidebar-navigation/SidebarNavigation.tsx
@@ -1,27 +1,39 @@
+"use client"
+
 import Link from "next/link";
 import IconSidebar from "../icons/icon-sidebar/IconSidebar";
 import IconGrid from "../icons/icon-grid/IconGrid";
 import IconBook from "../icons/icon-book/IconBook";
 import IconSettings from "../icons/icon-settings/IconSettings";
 import IconLogout from "../icons/icon-logout/IconLogout";
+import { useState } from "react";
 
 export const SidebarNavigation = () => 
 {
-    return(<aside className="sidebar">
+
+    const [sidebarToggle,setSidebarToggle] = useState(false);
+
+    const toggleSidebar = () => {
+        console.log('Toggle Class',sidebarToggle)
+        setSidebarToggle(!sidebarToggle)
+    }
+
+    return(<aside className={`sidebar${sidebarToggle ? "" : "--expanded"}`}>
         <div className="sidebar__container">
             <div className="sidebar__container-top">
 
                 <div className="sidebar__container-icon">
                 </div>
 
-                <div className="sidebar__container-name">
+                <div className={`sidebar__container-name${sidebarToggle ? "" : "--expanded"}`}>
                     <h2>Study App</h2>
                 </div>
 
-                <div className="sidebar__container-menu-toggle">
-                    <IconSidebar width={32} height={32} />
+                <div className={`sidebar__container-menu-toggle`}>
+                    <button onClick={toggleSidebar}>
+                        <IconSidebar width={32} height={32} />
+                    </button>
                 </div>
-
             </div>
 
             <div className="sidebar__container-middle">
