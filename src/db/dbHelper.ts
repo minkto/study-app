04fee9ca1e13c58ca05f3d75befc3e3e47e2d53c
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-05-27 14:26:51 +0100

    [SA-68]: Mase change to cascade delete of Resources and it's associated chapters.

--- src/db/dbHelper.ts
+++ src/db/dbHelper.ts
@@ -16,6 +16,39 @@ export async function queryData(text: string, params?: any[]) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function queryWithTranscation(dbQueryCollection: DbQuery[]) :Promise<boolean>
+{
+    const client = await pool.connect();
+
+    try
+    {
+        await client.query('BEGIN');
+        for(const dbQuery of dbQueryCollection) 
+        {
+            await client.query(dbQuery.text,dbQuery.values);
+        }
+        await client.query('COMMIT');
+        return true;
+    }
+    catch(error)
+    {
+        console.log('An error has occured with a SQL transaction : ', error);
+        
+        try {
+            await client.query('ROLLBACK');
+        } catch (rollbackError) {
+            console.error('Rollback failed:', rollbackError);
+        }
+
+        throw error;
+    }
+    finally
+    {
+        client.release();
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function queryDataSingleRow(text: string, params?: any[]) {
     const client = await pool.connect();
     try {
