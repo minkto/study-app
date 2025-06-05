d426e2c08f98bc13d2983a459ef0e65a20b04bb8
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-06-05 16:34:59 +0100

    [SA-106]: Made changes to use TanStack table for ResourceListings.

--- src/components/listings-search-bar/ListingsSearchBar.tsx
+++ src/components/listings-search-bar/ListingsSearchBar.tsx
@@ -1,19 +1,16 @@
-import Link from "next/link";
 import ListingsSearchBox from "../listings-search-box/ListingsSearchBox";
 import ListingsSearchFilterOptions from "../listings-search-filter-options/ListingsSearchFilterOptions";
-import IconPlus from "../icons/icon-plus/IconPlus";
 
-interface ListingsSearchBarProps 
-{
-    onSearchSubmit? : () => void    
+interface ListingsSearchBarProps {
+    onSearchSubmit?: () => void;
+    children?: React.ReactNode;
 }
 
-export const ListingsSearchBar = ({onSearchSubmit} : ListingsSearchBarProps) => 
-{
-    return ( <div className='dashboard__widget-row'>
-        <ListingsSearchBox  onSearchSubmit={onSearchSubmit}/>
-        <ListingsSearchFilterOptions/>
-        <Link className='dashboard-primary-btn' href={'chapters/add-chapter'}><IconPlus width={24} height={24} />Add</Link>
+export const ListingsSearchBar = ({ onSearchSubmit, children }: ListingsSearchBarProps) => {
+    return (<div className='dashboard__widget-row'>
+        <ListingsSearchBox onSearchSubmit={onSearchSubmit} />
+        <ListingsSearchFilterOptions />
+        {children}
     </div>)
 }
 
--- src/components/resource-listings/ResourceListings.tsx
+++ src/components/resource-listings/ResourceListings.tsx
@@ -1,30 +1,62 @@
 import { GetResourceDto } from '@/shared.types';
 import ResourceListingsCard from '../resource-listings-card/ResourceListingsCard';
 import styles from './resource-listings.module.css'
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { createColumnHelper, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import ListingsSearchBar from '../listings-search-bar/ListingsSearchBar';
+import Link from 'next/link';
+import IconPlus from '../icons/icon-plus/IconPlus';
+import { useDataTableQueryParams } from "@/hooks/useDataTableQueryParams";
 
 const ResourceListings = () => {
 
-  const [resources, setResources] = useState<GetResourceDto[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const { sorting, constructQueryString } = useDataTableQueryParams();
 
-  const getResources = async () => {
+  const [data, setData] = useState<GetResourceDto[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const columnHelper = createColumnHelper<GetResourceDto>();
+
+  const columns = [
+    columnHelper.accessor('name', {
+      cell: info => info.getValue(),
+      header: () => <span>Name</span>
+    }),
+    columnHelper.accessor('categoryId', {
+      cell: info => info.getValue(),
+      header: () => <span>Category</span>
+    })
+
+  ];
+
+  const table = useReactTable({
+    columns,
+    data,
+    enableMultiSort: false,
+    state:
+    {
+      sorting
+    },
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const getResources = useCallback(async () => {
     setLoading(true);
     try {
-      const response = await fetch('/api/resources');
+      const queryString = constructQueryString();
+      const response = await fetch(`/api/resources${queryString}`);
 
       if (!response.ok) {
         throw new Error(`Failed to fetch resources: ${response.status}`);
       }
       const data = await response.json();
-      setResources(data);
+      setData(data);
 
     } catch (error) {
       console.error("An error has occurred while getting the resources: ", error);
     } finally {
       setLoading(false);
     }
-  };
+  }, [constructQueryString]);
 
   const deleteResource = async (id: number | undefined): Promise<void> => {
     setLoading(true);
@@ -45,14 +77,27 @@ const ResourceListings = () => {
     }
   }
 
-
   useEffect(() => {
     getResources();
-  }, [])
+  }, [getResources]);
 
-  return loading ? <p>Loading...</p> : (<div className={styles["resources-listing"]}>
-    {resources?.map(r => <ResourceListingsCard onDelete={deleteResource} key={r.resourceId} resource={r} />)}
-  </div>)
+
+  return loading ? <p>Loading...</p> : (
+
+    <div className={styles["resources-listing-wrapper"]}>
+      <ListingsSearchBar>
+        <Link className='dashboard-primary-btn' href={'resources/add-resource'}><IconPlus width={24} height={24} />Add</Link>
+      </ListingsSearchBar>
+
+      <div className={styles["resources-listing"]}>
+        {table.getRowModel().rows.map(r => (
+          <ResourceListingsCard
+            onDelete={deleteResource}
+            key={r.original.resourceId}
+            resource={r.original} />)
+        )}
+      </div>
+    </div>)
 }
 
 export default ResourceListings;
