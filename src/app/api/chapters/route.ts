8e976bd42e834e1efa7140422cd8055593f0e6e4
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-07-14 10:47:44 +0100

    [SA-136]:Added ability to detect if user owns resource and set POST protection.

--- src/app/api/chapters/route.ts
+++ src/app/api/chapters/route.ts
@@ -1,11 +1,20 @@
 import { createChapter } from "@/db/chapters/createChapter";
+import resourceWithUserExists from "@/db/resources/resourceWithUserExists";
 import { validateChapter } from "@/services/validateChaptersService";
 import { Chapter } from "@/shared.types";
+import { isStringEmpty } from "@/utils/stringUtils";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
 
     try {
+        const { userId } = await auth();
+
+        if (isStringEmpty(userId)) {
+            return new Response("Unauthorized", { status: 401 });
+        }
+
         const res = await request.json();
         const chapter: Chapter =
         {
@@ -14,13 +23,17 @@ export async function POST(request: Request) {
             statusId: res["statusId"],
             url: res["url"],
             originalDateCompleted: res["originalDateCompleted"],
-            lastDateCompleted : res["lastDateCompleted"]
+            lastDateCompleted: res["lastDateCompleted"]
         };
 
         const validationModel = validateChapter(chapter);
-        if(!validationModel.isValid)
-        {
-            return NextResponse.json({message: validationModel.message}, { status: 400 });
+        if (!validationModel.isValid) {
+            return NextResponse.json({ message: validationModel.message }, { status: 400 });
+        }
+
+        const resourceExists = await resourceWithUserExists(res["resourceId"], userId);
+        if (!resourceExists) {
+            return NextResponse.json({ message: "Forbidden: You do not own this resource." }, { status: 403 });
         }
 
         const result = await createChapter(chapter);
