391ce8e2d6858ca09997422b6717357a149bf0e7
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-08-19 14:25:40 +0100

    [SA-152]: Added request box message.

--- src/components/ai-chat-container/AIChatContainer.tsx
+++ src/components/ai-chat-container/AIChatContainer.tsx
@@ -1,12 +1,30 @@
+"use client";
+
+import { useState } from "react";
 import AIChatWindow from "../ai-chat-window/AIChatWindow";
 import AISearchBar from "../ai-search-bar/AISearchBar";
 import styles from "./ai-chat-container.module.css";
 
 const AIChatContainer = () => {
+
+    const [requestMessages, setRequestMessages] = useState<string[]>([]);
+ 
+    const handleOnSubmit = (text: string) => {
+        const inputValue = text?.trim() || "";
+
+        if( inputValue !== "") {
+            addRequestMessage(inputValue);
+        }   
+    };
+
+    const addRequestMessage = (text: string) => {
+        setRequestMessages(prevMessages => [...prevMessages, text]);
+    }
+
     return (
         <div className={styles["ai-chat-container"]}>
-            <AISearchBar />
-            <AIChatWindow />
+            <AISearchBar onSearchSubmit={handleOnSubmit} />
+            <AIChatWindow messages={requestMessages}   />
         </div>
     );
 }
--- src/components/ai-chat-window/AIChatWindow.tsx
+++ src/components/ai-chat-window/AIChatWindow.tsx
@@ -1,10 +1,20 @@
 import styles from "./ai-chat-window.module.css";
 
-const AIChatWindow = () => 
-{
+interface AIChatWindowProps {
+    messages?: string[];
+}
+
+const AIChatWindow = ({ messages }: AIChatWindowProps) => {
     return (
-    <div className={styles["ai-chat-window"]}>
-    </div>)
+        <div className={styles["ai-chat-window"]}>
+            {messages && messages.length > 0 ? (messages.map((message, index) =>
+            (
+                <div key={index} className={styles["ai-chat-window-request"]}>
+                    <p className={styles["ai-chat-window-request__message"]}>{message}</p>
+                </div>))) : null}
+        </div>
+    )
+
 }
 
 
