d57633770f127614e032fb2cd94771fe5a32dd03
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-08-26 11:43:36 +0100

    [SA-154]:Added Open AI Service to hold ai logic.

---
+++ src/services/openAIService.ts
@@ -0,0 +1,69 @@
+import OpenAI from "openai";
+import { zodToJsonSchema } from "openai/_vendor/zod-to-json-schema/zodToJsonSchema.mjs";
+import z from "zod";
+
+export async function getResourceFromOpenAI(prompt: string) {
+    try {
+        const client = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        let resources = {};
+
+        // TODO: Define the schema for the request body.
+        const ResourceObject = z.object({
+            name: z.string(),
+            chapters: z.array(z.string()),
+            source: z.string(),
+        });
+
+        const ResourceListObject = z.object({
+            resources: ResourceObject.array(),
+        })
+
+        const resourceSchema = zodToJsonSchema(ResourceListObject);
+
+        const response = await client.responses.create({
+            model: 'gpt-4o-mini',
+            input: [
+                {
+                    role: "system",
+                    content:
+                        `Create five Resource objects and add all chapters and their sub chapters.
+    Format citation source into a valid URL string.
+    Only get resource if the source does not have a 404 status code.
+    Format the JSON to make it valid matching the schema structure exactly.
+    Remove new lines in chapter name property.
+    Escape all double quotes in the JSON.
+    If you do not understand the prompt, return an empty JSON object { "resources": [] }.`
+                },
+                { role: "user", content: `Find resources to learn ${prompt}. Include the name of the resource. Drill down on all the topics as chapters.` },
+
+            ],
+            text: {
+                format: {
+                    type: "json_schema",
+                    name: "resource_list_object",
+                    schema: resourceSchema
+                },
+            },
+            tools: [{ type: "web_search_preview" }],
+        })
+
+
+        try {
+            resources = JSON.parse(response.output_text);
+            return resources;
+        } catch (err) {
+            console.error("Raw output was not valid JSON:", err);
+        }
+
+        return { resources: [] }; // Return an empty array if parsing fails
+    }
+
+    // TODO: Handle specific errors from OpenAI service.
+    catch (error) {
+        console.error("API error:", error);
+        return new Response("Internal Server Error", { status: 500 });
+    }
+}
