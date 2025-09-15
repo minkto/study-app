0767f900d3ef43acdd356b710aa69b1956227d9c
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-09-15 10:43:22 +0100

    [SA-186]: Fixed review days before chapter is due.

--- src/db/reports/getChaptersWithLongestReviewDates.ts
+++ src/db/reports/getChaptersWithLongestReviewDates.ts
@@ -1,19 +1,34 @@
 import { queryData } from "../dbHelper";
 
 export const getChaptersWithLongestReviewDates = async (userId: string) => {
+    
+    const daysBeforeReviewDue = process.env.DEFAULT_DAYS_BEFORE_CHAPTER_REVIEW_DUE ?? 30
+    
     try {
         const query =
-            `SELECT  
+            `WITH params AS (
+                SELECT (CURRENT_DATE AT TIME ZONE 'UTC')::date AS today
+            ),
+            resource_stats AS (
+                SELECT  
                     r.name AS resource_name,
-                    c.name  AS chapter_name,
-                    COALESCE(CAST((CURRENT_TIMESTAMP AT TIME ZONE 'UTC')::date - (last_date_completed AT TIME ZONE 'UTC')::date  AS integer), 0) AS days_since_last_completed   
-                FROM chapters  c 
+                    c.name AS chapter_name,
+                (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')::date  - (last_date_completed AT TIME ZONE 'UTC')::date   AS days_since_last_completed
+                FROM chapters c 
                 INNER JOIN resources r ON c.resource_id = r.resource_id
+                CROSS JOIN params p
                 WHERE r.user_id = $1
-                ORDER BY days_since_last_completed DESC
-                LIMIT 4;`
+                AND (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')::date  - (last_date_completed AT TIME ZONE 'UTC')::date > $2
+            )
+            SELECT 
+                resource_name, 
+                chapter_name, 
+                days_since_last_completed
+            FROM resource_stats
+            ORDER BY days_since_last_completed DESC
+            LIMIT 4;`
 
-        const result = await queryData(query, [userId]);
+        const result = await queryData(query, [userId, daysBeforeReviewDue]);
 
         const dataResult = result.map(r => {
             return {
@@ -28,5 +43,4 @@ export const getChaptersWithLongestReviewDates = async (userId: string) => {
     catch (error) {
         console.log("An error has occured in the database: ", error);
     }
-
 }
