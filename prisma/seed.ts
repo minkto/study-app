325d5666b01fec227af635ea6abb3d09d94c2032
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-09-17 11:48:11 +0100

    [SA-192]: Added database app schema and seeding logic.

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient, Prisma } from "../src/generated/prisma/client";
+
+const prisma = new PrismaClient();
+const statusData: Prisma.StatusesCreateInput[] = [
+    { status_id: 0, name: 'Not Started' },
+    { status_id: 1, name: 'In Progress' },
+    { status_id: 2, name: 'Completed' }
+]
+
+export async function main() {
+    for (const status of statusData) {
+        await prisma.statuses.create({ data: status });
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    });
