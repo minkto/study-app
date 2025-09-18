542f0e1f4ff36b1ea0e83e0fa9bc889050f89f60
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-09-18 20:05:51 +0100

    [SA-197]: Fixed suspense error on run build, that is caused by useSearchParams() in children.

--- src/app/dashboard/resources/page.tsx
+++ src/app/dashboard/resources/page.tsx
@@ -2,14 +2,17 @@
 
 import { DashboardWidget } from '@/components/dashboard/DashboardWidget';
 import ResourceListings from '@/components/resource-listings/ResourceListings';
+import { Suspense } from 'react';
 
 export default function Page() {
 
     return (
-        <div>
-            <h1>Resources Page</h1>
-            <DashboardWidget title="Resources">
-                <ResourceListings />
-            </DashboardWidget>
-        </div>)
+        <Suspense fallback={<>...</>}>
+            <div>
+                <h1>Resources Page</h1>
+                <DashboardWidget title="Resources">
+                    <ResourceListings />
+                </DashboardWidget>
+            </div>
+        </Suspense>)
 }
