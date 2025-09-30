bc67214b0a357f7efb576bda18539992114fde52
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-09-30 11:38:23 +0100

    [SA-200]: Added compose file and configuration to build next.js in docker.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    env: {
-      CHAPTERS_MAX_PAGE_SIZE: process.env.CHAPTERS_MAX_PAGE_SIZE,
-      RESOURCES_MAX_PAGE_SIZE: process.env.RESOURCES_MAX_PAGE_SIZE,
-      DEFAULT_DAYS_BEFORE_CHAPTER_REVIEW_DUE : process.env.DEFAULT_DAYS_BEFORE_CHAPTER_REVIEW_DUE
-    }
+  env: {
+    CHAPTERS_MAX_PAGE_SIZE: process.env.CHAPTERS_MAX_PAGE_SIZE,
+    RESOURCES_MAX_PAGE_SIZE: process.env.RESOURCES_MAX_PAGE_SIZE,
+    DEFAULT_DAYS_BEFORE_CHAPTER_REVIEW_DUE: process.env.DEFAULT_DAYS_BEFORE_CHAPTER_REVIEW_DUE
+  }, 
+  output: "standalone"
+
 };
 
 export default nextConfig;
