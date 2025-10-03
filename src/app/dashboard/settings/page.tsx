2dbddf5fc6365dfcc9dd7bd3887f02a5606c205a
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-10-03 15:34:08 +0100

    [SA-206]: Added Settings page.

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,17 @@
+import { DashboardWidget } from "@/components/dashboard/DashboardWidget";
+
+export default async function Page() {
+    return (
+        <div>
+            <h1>Settings</h1>
+            <DashboardWidget title="General">
+                <p>This will be where general settings are changed.</p>
+            </DashboardWidget>
+            <DashboardWidget title="Profile">
+                <p>This will be where profile specific settings are changed.</p>
+            </DashboardWidget>
+            <DashboardWidget title="Category">
+                <p>This will be where user category settings are changed.</p>
+            </DashboardWidget>
+        </div>)
+}
