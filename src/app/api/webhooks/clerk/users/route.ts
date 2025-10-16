1320208db9fd9dd31e6f649b2fd8a7433f17f294
paul.achikeh <p_achikeh@yahoo.co.uk>
2025-10-16 12:25:45 +0100

    [SA-217]: Added create user from clerk functionality.

--- src/app/api/webhooks/clerk/users/route.ts
+++ src/app/api/webhooks/clerk/users/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { createUserFromClerk } from "@/db/users/createUser";
 
 export async function POST(request: NextRequest) {
 
-    // Get the webhook secret - fail fast if not configured
     const webhookSecret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
 
     if (!webhookSecret) {
-        console.error("CLERK_SYNC_WEBHOOK_SECRET is not configured");
+        console.error("CLERK_WEBHOOK_SIGNING_SECRET is not configured");
         return NextResponse.json(
             { error: "Webhook secret not configured" },
             { status: 500 }
@@ -16,18 +16,25 @@ export async function POST(request: NextRequest) {
 
     try {
         const webhookEvent = await verifyWebhook(request, { signingSecret: webhookSecret });
-        if (webhookEvent.type === 'user.created') {
-            //TODO: Sync up the users.
-            console.log("Webhook event: ", webhookEvent);
-            console.log("Syncing - user id: ", webhookEvent?.data?.id);
-            console.log("Http Request: ", request);
-
-            return NextResponse.json({ success: true }, { status: 200 })
+        switch (webhookEvent.type) {
+            case 'user.created':
+                return handleUserCreation(webhookEvent?.data?.id);
+            default:
+                return NextResponse.json({ message: 'Event type not handled' }, { status: 400 });
         }
 
     } catch (err) {
-        return NextResponse.json({ message: 'Bad Request', error: err instanceof Error ? err.message : err })
+        return NextResponse.json({ message: 'Bad Request', error: err instanceof Error ? err.message : err }, { status: 404 });
     }
+}
 
-    return NextResponse.json({ message: 'Bad Request' }, { status: 400 })
+async function handleUserCreation(clerkUserId: string) {
+    const result = await createUserFromClerk(clerkUserId);
+    if (result && result > 0) {
+        console.log("Clerk User created in DB with id: ", clerkUserId);
+        return NextResponse.json({ success: true }, { status: 200 })
+    } else {
+        console.error("Failed to create User from Clerk with Clerk id: ", clerkUserId);
+        return NextResponse.json({ message: 'Failed to create user' }, { status: 500 });
+    }
 }
