85732b3d18a30b3f7240b66a06d391df2fc7fe9f
paul.achikeh <p_achikeh@yahoo.co.uk>
2026-04-17 17:27:16 +0100

    [SA-272]: Style changes made for table buttons and primary dashboard buttons.

--- src/components/icons/icon-plus/IconPlus.tsx
+++ src/components/icons/icon-plus/IconPlus.tsx
@@ -2,8 +2,10 @@ import { SvgIcon } from "@/shared.types";
 
 const IconPlus = ({width,height}: SvgIcon) => 
 {
-    return(<svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 5V19M5 12H19" stroke="#1E1E1E" strokeWidth={"2.5"}  strokeLinecap="round" strokeLinejoin="round"/>
+    return(<svg className="icon-img" width={width} height={height} viewBox={`0 0 ${width} ${height}`} 
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path className="icon-img__path" d="M12 5V19M5 12H19" strokeWidth={"2.5"} 
+         strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
         )
 }
