26287a10ce900ccf545cbd0a4327f4268c9afbc0
paul.achikeh <p_achikeh@yahoo.co.uk>
2026-05-29 07:09:41 +0100

    [SA-274]: Refactured Days Since complete pill into component.

---
+++ src/components/resource-chapters-listings/ChapterDaysCompletedPill.tsx
@@ -0,0 +1,15 @@
+import styles from "./chapter-days-completed.module.css";
+import IconClock from "../icons/icon-clock/IconClock";
+
+const ChapterDaysCompletedPill = ({ days }: { days: number }) => {
+    return (
+        <div className={`${styles["chapter-days-complete-pill__completion-section"]}`}>
+            <p>Last Reviewed</p>
+            <div className={styles["chapter-days-complete-pill__days-completed"]}>
+                <IconClock className={styles["chapter-days-complete-pill__days-completed-icon"]} width={32} height={32} />
+                <div >{days} Day(s)</div>
+            </div>
+        </div>)
+}
+
+export default ChapterDaysCompletedPill;
