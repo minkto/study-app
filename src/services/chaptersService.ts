f901d4e4b9f4f00eee2ceec77ac439f3315e86b3
paul.achikeh <p_achikeh@yahoo.co.uk>
2026-06-11 07:19:23 +0100

    [SA-277]: Added Chapter notes to service.

--- src/services/chaptersService.ts
+++ src/services/chaptersService.ts
@@ -1,8 +1,12 @@
 import { getChapter } from "@/db/chapters/getChapter"
+import getNotes from "@/db/chapters/notes/getNotes";
 import { Chapter } from "@/shared.types";
 
-export const getChapterDetails = async(chapterId: number, userId: string): Promise<Chapter | null> => 
-{
-    const chapter = await getChapter(chapterId,userId);
+export const getChapterDetails = async (chapterId: number, userId: string): Promise<Chapter | null> => {
+    const chapter = await getChapter(chapterId, userId);
+    if (chapter) {
+        chapter.notes = await getNotes(chapterId, userId);
+    }
+
     return chapter;
 }
