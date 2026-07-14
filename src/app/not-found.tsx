2df2cc68175cb8952ed91ff1aa0ab886136b885b
paul.achikeh <p_achikeh@yahoo.co.uk>
2026-07-14 08:40:39 +0100

    [SA-319]: Added not found page 404.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './not-found.module.css'
+
+export default function NotFound() {
+  return (
+    <div className={styles["not-found-container"]}>
+      <Image src="/avatar/learn-lobe-404-image.svg" alt="404" width={200} height={200} />
+      <h1 className={styles["not-found-title"]}>404</h1>
+      <h2 className={styles["not-found-subtitle"]}>Oops! We could not find the page you are looking for. Let&apos;s go back to the dashboard.</h2>
+      <Link className={styles["not-found-link"]} href="/dashboard">Return to Dashboard</Link>
+    </div>
+  )
+}
