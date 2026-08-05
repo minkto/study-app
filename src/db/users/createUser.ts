8430306114d556721ea694e567300b6ac0f73f99
paul.achikeh <p_achikeh@yahoo.co.uk>
2026-08-05 15:55:45 +0100

    [SA-345]: Made changes to db user and user settings functions to create and return values for use.

--- src/db/users/createUser.ts
+++ src/db/users/createUser.ts
@@ -1,25 +1,34 @@
-import { User } from "@/shared.types";
+import { AppUser } from "@/shared.types";
 import { queryDataSingleRow } from "../dbHelper";
 
-export async function createUserFromClerk(clerkUserId: string): Promise<User | undefined| null> {
+export async function createUserFromClerk(clerkUserId: string): Promise<AppUser | undefined | null> {
     try {
-        let user: User | null = null;
-        const query =
-        {
-            text: "INSERT INTO users(clerk_user_id)VALUES($1) RETURNING *;",
-            values: [clerkUserId]
+        const query = {
+            text: `
+            INSERT INTO users (clerk_user_id)
+            VALUES ($1)
+            ON CONFLICT (clerk_user_id)
+            DO UPDATE SET clerk_user_id = EXCLUDED.clerk_user_id
+            RETURNING user_id, clerk_user_id
+        `,
+            values: [clerkUserId],
         };
 
-        const userResult = await queryDataSingleRow(query.text, query.values);
-        if (userResult) {
-            user =
-            {
-                userId: userResult.user_id,
-                clerkUserId: userResult.clerk_user_id
-            }
+        const userResult = await queryDataSingleRow(
+            query.text,
+            query.values
+        );
+
+        if (!userResult) {
+            throw new Error(
+                `Failed to ensure user for Clerk user ${clerkUserId}`
+            );
         }
 
-        return user;
+        return {
+            userId: userResult.user_id,
+            clerkUserId: userResult.clerk_user_id,
+        };
     }
     catch (error) {
         console.error("Database error:", { message: 'Database error', error: error instanceof Error ? error.message : error });
