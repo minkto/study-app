ee701f0505649c00fa4c977361bde75865b9ea0b
paul.achikeh <p_achikeh@yahoo.co.uk>
2026-09-01 11:28:14 +0100

    [SA-362]: Added validation to test all widgets for dashboards.

--- e2e/tests/dashboard/dashboard.spec.ts
+++ e2e/tests/dashboard/dashboard.spec.ts
@@ -10,4 +10,18 @@ test('check main dashboard page with url rewrite: / ', async ({ page, baseURL })
     // Navigate to the main dashboard page (protected page)
     await dashboardPage.goToDashboardPageUrlRewrite();
     await expect(page).toHaveTitle("Dashboard | LearnLobe");
-})
+});
+
+test('dashboard renders all cards', async ({ page, baseURL }) => {
+    const dashboardPage = new DashboardPage(page, baseURL);
+    await dashboardPage.goToDashboardPage();
+
+    await expect(page).toHaveTitle("Dashboard | LearnLobe");
+    await expect(page.getByText("Completed Today")).toBeVisible();
+    await expect(page.getByText("Completed This Month")).toBeVisible();
+    await expect(page.getByText("In Progress")).toBeVisible();
+    await expect(page.getByText("Latest Resources")).toBeVisible();
+    await expect(page.getByText("Top 4 Categories")).toBeVisible();
+    await expect(page.getByText("Chapters With Review Dates Due")).toBeVisible();
+    await expect(page.getByText("Top Pinned Resources")).toBeVisible();
+});
